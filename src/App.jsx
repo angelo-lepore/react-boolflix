5aca09e47d2d515fa37eb504f128d0a0a8566936
angelo-lepore <angelo.lepore94@gmail.com>
2025-07-29 11:27:34 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// Import di Bootstrap (CSS, JS e icone)
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+// Componente principale dell'applicazione
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Header</h1>
+      </header>
+      <main>
+        <h3>Main</h3>
+      </main>
+    </>
+  );
+}
+
+export default App;
